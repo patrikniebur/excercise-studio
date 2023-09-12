f5490f58c88f7b752eda603cd3eee77d214cda57
Patrik Niebur <patrik.niebur@gmail.com>
2023-09-12 12:04:04 +0100

    Add offline use

--- src/sw.ts
+++ src/sw.ts
@@ -3,15 +3,16 @@
 // export empty type because of tsc --isolatedModules flag
 export type {};
 declare const self: ServiceWorkerGlobalScope;
+import { manifest, version } from "@parcel/service-worker";
 
 import { MessageToSW, MessageFromSW } from "./ts/helpers/serviceWorkerTools";
 import { storeHandle, getHandle } from "./ts/helpers/storage";
 
 const DEBUG = true;
 
-function debug(message: unknown) {
+function debug(...message: unknown[]) {
   if (DEBUG) {
-    console.log(message);
+    console.log(...message);
   }
 }
 
@@ -19,9 +20,39 @@ const runtimeStorage = {
   handles: [] as FileSystemDirectoryHandle[],
 };
 
-self.addEventListener("activate", () => {
-  debug("Claiming clients...");
-  self.clients.claim();
+/* MAKE STUFF AVAILABLE OFFLINE */
+async function installCaches() {
+  const cache = await caches.open(version);
+  debug({ manifest });
+  await cache.addAll(manifest);
+}
+self.addEventListener("install", (e) => e.waitUntil(installCaches()));
+
+async function activate() {
+  const keys = await caches.keys();
+  await Promise.all([
+    ...keys.map((key) => key !== version && caches.delete(key)),
+    self.clients.claim(),
+  ]);
+}
+
+self.addEventListener("activate", (e) => e.waitUntil(activate()));
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.open(version).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        debug("Network", { response, request: event.request });
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    }),
+  );
 });
 
 self.addEventListener("message", async (e) => {
